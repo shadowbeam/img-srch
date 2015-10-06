c7830adafbc0a61cf6e6a5d6f42bbfcf800f3ed2
and <allan.watson@tsgforce.com>
2015-10-06 17:41:44 +0100

    retrieve file data

--- assets/js/img-srch.js
+++ assets/js/img-srch.js
@@ -19,31 +19,43 @@
   });
 
 
-
   var SelectBox = React.createClass({
-
-
-
-   handleFileDrop: function(e){
-     fileName = e.target.value.split( '\\' ).pop();
-     this.setState({labelValue : fileName + ' selected'});
-   },
-
    getInitialState: function(){
     return{
-      labelValue: "Choose a file"
+      labelValue: "Choose a file",
+      data_uri: null
+
     };
   },
 
-  render: function() {
-    return ( 
-      <div className="select-box">
-      <input type="file" name="file" id="file" className="select-box--inputfile" onChange={this.handleFileDrop} />
-      <label htmlFor="file">{this.state.labelValue}</label> 
-      </div>
-      );
-  }
+  handleFileDrop: function(e){
+   fileName = e.target.value.split( '\\' ).pop();
+   this.setState({
+    labelValue : fileName + ' selected',
+    data_uri : e.target.result
+  });
+   this.uploadFile();
 
+ },
+
+ uploadFile: function(){
+
+  var request = new XMLHttpRequest();
+  request.open('POST', '/my/url', true);
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  request.send(this.state.data_uri);
+
+},
+
+
+render: function() {
+  return ( 
+    <div className="select-box">
+    <input type="file" name="file" id="file" className="select-box--inputfile" onChange={this.handleFileDrop} />
+    <label htmlFor="file">{this.state.labelValue}</label> 
+    </div>
+    );
+}
 
 });
 
