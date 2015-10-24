482736c5edff4892d7f132300dba39ee2520829b
and <allan.watson@tsgforce.com>
2015-10-24 20:10:07 +0100

    encode html

--- assets/js/img-srch.js
+++ assets/js/img-srch.js
@@ -137,11 +137,16 @@ var GooglePane = React.createClass({
 });
 
 var ImageResult = React.createClass({
+
+    createMarkup: function(html){ 
+        return {__html: html};
+    },
+
     render: function() {
         return <div className='image-result'>
         <h2><a href={this.props.url}>{this.props.title}</a></h2>
-        <h3>{this.props.cite}</h3>
-        <p>{this.props.description}</p>
+        <h3 dangerouslySetInnerHTML={this.createMarkup(this.props.cite)}></h3>
+        <p dangerouslySetInnerHTML={this.createMarkup(this.props.description)}></p>
         </div>;
     }
 });
